22b359d246455f1702a27727941aa922e92edcbf
Your Name <you@example.com>
2017-12-22 19:50:15 -0500

    add pz_lb_515

--- pz_script/create_small.py
+++ pz_script/create_small.py
@@ -22,13 +22,15 @@ logger.addHandler(handler)
 logger.info('start')
 
 #Load Train Data
-#dtypes = {'id':'uint32', 'item_nbr':'int32', 'store_nbr':'int8', 'unit_sales':'float32'}
+dtypes = {'id':'uint32', 'item_nbr':'int32', 'store_nbr':'int8', 'unit_sales':'float32'}
 #train = pd.read_csv('../input/train.csv', usecols=[1,2,3,4,5], dtype=dtypes, parse_dates=['date'] )
 
 train = pd.read_csv('../input/train.csv', dtype=dtypes, parse_dates=['date'])
 
 logger.info('load train data successful')
-train_small = train.loc[(train.item_nbr==103501),]
+#train_small = train.loc[(train.item_nbr==2116416 | train.item_nbr==2113914 ),]
+
+train_small = train.loc[(train.store_nbr==1 ),]
 
 
 #Load Test Data
@@ -36,10 +38,10 @@ train_small = train.loc[(train.item_nbr==103501),]
 test = pd.read_csv('../input/test.csv', dtype=dtypes, parse_dates=['date'])
 logger.info('load test data')
 
-test_small = test.loc[(test.item_nbr==103501),]
+test_small = test.loc[(train.store_nbr==1),]
 
 #Write File
-test_small.to_csv('../input/test_small.csv', index=False)
-train_small.to_csv('../input/train_small.csv', index=False)
+test_small.to_csv('../input/test_1s.csv', index=False)
+train_small.to_csv('../input/train_1s.csv', index=False)
 
 logger.info('end')
