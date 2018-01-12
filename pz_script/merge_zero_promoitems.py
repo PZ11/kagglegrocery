c257f44721a61840e54c6da6c1f722b8541aefe0
pz11 <pz201311@gmail.com>
2018-01-12 18:27:56 -0500

    Add_mergezeropromoitem.py

---
+++ pz_script/merge_zero_promoitems.py
@@ -0,0 +1,34 @@
+
+"""
+This is an upgraded version of Ceshine's LGBM starter script, simply adding
+more average features and weekly average features on it.
+"""
+from datetime import date, timedelta
+
+import pandas as pd
+import numpy as np
+import sys
+
+##########################################################################
+
+df_sub = pd.read_csv('../input/zero_promo_testset.csv')
+
+df_sub['unit_sales'] = 0
+
+df_prev = pd.read_csv('../submit/T280.csv')
+
+t_new = pd.merge(df_prev, df_sub, on=['id'], how='left')
+t_new['unit_sales'] = t_new.unit_sales_y.combine_first(t_new.unit_sales_x)
+
+submission = t_new[['id', 'unit_sales']]
+del t_new
+
+print("Merged  SUM =",  submission.unit_sales.sum())
+print("Merged  MEAN =",  submission.unit_sales.mean())
+
+submission.to_csv('../submit/T280_V2.csv.gz',
+                  float_format='%.4f', index=None, compression='gzip')
+
+
+
+
